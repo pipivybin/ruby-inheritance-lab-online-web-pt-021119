c75e401f5376d6db085ffc1f4c389f7f35442203
Ming Duan <11203duan@gmail.com>
2019-03-25 16:21:17 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,10 @@
 class User
 
-def initialize(firstname, lastname)
+  attr_reader :first_name, :last_name
+
+def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
 
 end
 
